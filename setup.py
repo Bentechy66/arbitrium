bc2b5ed20b6bd8d3fd5279e342a97c02d93eae8b
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-10-21 20:06:55 +0100

    Change Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'arbitrium',
   packages = ['arbitrium'],
-  version = '1.1.2',
+  version = '1.1.3',
   license='MIT',
   description = 'Arbitrium - The Easy Menu System For Python',
   long_description = "Arbitrium is an easy-to-use menu system for Python 3.5.1 and above. For usage, see https://github.com/bentechy66/arbitrium.",
